b0f6d2335067d09b342528903dfa124cb6584e0a
jeremy.zaire <jeremy.zaire@cdbdx.biz>
2019-10-07 21:52:15 +0200

    Ajout des scripts de bdd

---
+++ common/src/main/resources/create_script.sql
@@ -0,0 +1,19 @@
+CREATE TABLE product(
+    product_id INT PRIMARY KEY NOT NULL,
+    content VARCHAR(100),
+);
+
+CREATE TABLE product_hibernate(
+    product_id INT PRIMARY KEY NOT NULL,
+    content VARCHAR(100),
+);
+
+CREATE TABLE product_mybatis(
+    product_id INT PRIMARY KEY NOT NULL,
+    content VARCHAR(100),
+);
+
+CREATE TABLE product_springdata(
+    product_id INT PRIMARY KEY NOT NULL,
+    content VARCHAR(100),
+);
---
+++ common/src/main/resources/insert_script.sql
@@ -0,0 +1,19 @@
+INSERT INTO product (product_id, content)
+VALUES
+(1 'Cdiscount Product'),
+(2, 'Amazon Product');
+
+INSERT INTO product_hibernate (product_id, content)
+VALUES
+(1, 'Cdiscount Product'),
+(2, 'Amazon Product');
+
+INSERT INTO product_mybatis (product_id, content)
+VALUES
+(1, 'Cdiscount Product'),
+(2, 'Amazon Product');
+
+INSERT INTO product_springdata (product_id, content)
+VALUES
+(1, 'Cdiscount Product'),
+(2, 'Amazon Product');
